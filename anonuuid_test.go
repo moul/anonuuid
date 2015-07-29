0362a3bade5abea48dafa6a8a4af3127690fe882
Manfred Touron <m@42.am>
2015-07-29 12:22:05 +0200

    Updated tests

--- anonuuid_test.go
+++ anonuuid_test.go
@@ -5,17 +5,6 @@ import (
 	"testing"
 )
 
-var (
-	EXAMPLE_INPUT = `
-VOLUMES_0_SERVER_ID=15573749-c89d-41dd-a655-16e79bed52e0
-VOLUMES_0_SERVER_NAME=hello
-VOLUMES_0_ID=c245c3cb-3336-4567-ada1-70cb1fe4eefe
-VOLUMES_0_SIZE=50000000000
-ORGANIZATION=fe1e54e8-d69d-4f7c-a9f1-42069e03da31
-TEST=15573749-c89d-41dd-a655-16e79bed52e0
-`
-)
-
 func ExampleNew() {
 	anonuuid := New()
 	fmt.Println(anonuuid)
@@ -25,7 +14,14 @@ func ExampleNew() {
 
 func ExampleSanitize() {
 	anonuuid := New()
-	fmt.Println(anonuuid.Sanitize(EXAMPLE_INPUT))
+	input := `VOLUMES_0_SERVER_ID=15573749-c89d-41dd-a655-16e79bed52e0
+VOLUMES_0_SERVER_NAME=hello
+VOLUMES_0_ID=c245c3cb-3336-4567-ada1-70cb1fe4eefe
+VOLUMES_0_SIZE=50000000000
+ORGANIZATION=fe1e54e8-d69d-4f7c-a9f1-42069e03da31
+TEST=15573749-c89d-41dd-a655-16e79bed52e0`
+
+	fmt.Println(anonuuid.Sanitize(input))
 	// Output:
 	// VOLUMES_0_SERVER_ID=00000000-0000-0000-0000-000000000000
 	// VOLUMES_0_SERVER_NAME=hello
@@ -35,15 +31,39 @@ func ExampleSanitize() {
 	// TEST=00000000-0000-0000-0000-000000000000
 }
 
+func ExampleFakeUUID() {
+	anonuuid := New()
+	fmt.Println(anonuuid.FakeUUID("15573749-c89d-41dd-a655-16e79bed52e0"))
+	fmt.Println(anonuuid.FakeUUID("15573749-c89d-41dd-a655-16e79bed52e0"))
+	fmt.Println(anonuuid.FakeUUID("c245c3cb-3336-4567-ada1-70cb1fe4eefe"))
+	fmt.Println(anonuuid.FakeUUID("c245c3cb-3336-4567-ada1-70cb1fe4eefe"))
+	fmt.Println(anonuuid.FakeUUID("15573749-c89d-41dd-a655-16e79bed52e0"))
+	fmt.Println(anonuuid.FakeUUID("fe1e54e8-d69d-4f7c-a9f1-42069e03da31"))
+	// Output:
+	// 00000000-0000-0000-0000-000000000000
+	// 00000000-0000-0000-0000-000000000000
+	// 11111111-1111-1111-1111-111111111111
+	// 11111111-1111-1111-1111-111111111111
+	// 00000000-0000-0000-0000-000000000000
+	// 22222222-2222-2222-2222-222222222222
+}
+
 func BenchmarkNew(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		New()
 	}
 }
 
-func BenchmarkSanitize(b *testing.B) {
+func BenchmarkFakeUUID(b *testing.B) {
+	anonuuid := New()
 	for i := 0; i < b.N; i++ {
-		anonuuid := New()
-		anonuuid.Sanitize(EXAMPLE_INPUT)
+		anonuuid.FakeUUID("15573749-c89d-41dd-a655-16e79bed52e0")
+	}
+}
+
+func BenchmarkSanitize(b *testing.B) {
+	anonuuid := New()
+	for i := 0; i < b.N; i++ {
+		anonuuid.Sanitize("A: 15573749-c89d-41dd-a655-16e79bed52e0, B: c245c3cb-3336-4567-ada1-70cb1fe4eefe, A: 15573749-c89d-41dd-a655-16e79bed52e0")
 	}
 }
