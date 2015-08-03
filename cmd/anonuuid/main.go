a952c638f23ee834425a362b4397448113850e36
Manfred Touron <m@42.am>
2015-08-03 08:54:46 +0200

    Using cli to parse cmdline

--- cmd/anonuuid/main.go
+++ cmd/anonuuid/main.go
@@ -4,12 +4,26 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path"
 
+	"github.com/codegangsta/cli"
 	"github.com/moul/anonuuid"
 )
 
 // main is the entrypoint
 func main() {
+	app := cli.NewApp()
+	app.Name = path.Base(os.Args[0])
+	app.Author = "Manfred Touron"
+	app.Email = "https://github.com/moul"
+	app.Version = "1.0.0-dev"
+	app.Usage = "Anonymize UUIDs outputs"
+
+	app.Action = action
+	app.Run(os.Args)
+}
+
+func action(c *cli.Context) {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	anonuuid := anonuuid.New()
